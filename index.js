daa9d49c5b2b9cf86f4396b91bb7875be698182c
kthomas80 <kthomas80@dxc.com>
2022-01-10 13:39:39 -0600

    adds wallet and project

---
+++ index.js
@@ -0,0 +1,11 @@
+const {
+  Connection,
+  PublicKey,
+  clusterApiUrl,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  Transaction,
+  Account,
+} = require("@solana/web3.js");
+
+const newPai = new Keypair();
