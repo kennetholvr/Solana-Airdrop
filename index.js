fcf8ef6ef5b8da8f167cde064edd310cc87cf01d
kthomas80 <kthomas80@dxc.com>
2022-01-10 14:19:34 -0600

    updates node and adds driver

--- index.js
+++ index.js
@@ -8,6 +8,52 @@ const {
   Account,
 } = require("@solana/web3.js");
 
+//generates new wallet key-pair
 const newPai = new Keypair();
+
+//stores public and private key
 const publicKey = new PublicKey(newPair._keypair.publicKey).toString();
 const secretKey = newPair._keypair.secretKey;
+
+//gets wallet balance
+const getWalletBalance = async () => {
+  try {
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+    const myWallet = await Keypair.fromSecretKey(secretKey);
+    const walletBalance = await connection.getBalance(
+      new PublicKey(myWallet.publicKey)
+    );
+    console.log(`=> For wallet address ${publicKey}`);
+    console.log(
+      `   Wallet balance: ${
+        parseInt(walletBalance) / LAMPORTS_PER_SOL
+      }SOL`
+           );
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+//airdrops sol (in terms of lamports)
+const airDropSol = async () => {
+  try {
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+    const walletKeyPair = await Keypair.fromSecretKey(secretKey);
+    console.log(`-- Airdropping 2 SOL --`);
+    const fromAirDropSignature = await connection.requestAirdrop(
+      new PublicKey(walletKeyPair.publicKey),
+      2 * LAMPORTS_PER_SOL
+    );
+    await connection.confirmTransaction(fromAirDropSignature);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// for calling previous functions
+const driverFunction = async () => {
+  await getWalletBalance();
+  await airDropSol();
+  await getWalletBalance();
+}
+driverFunction();
